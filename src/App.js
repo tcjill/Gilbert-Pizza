6ecc184dcadd5447669f4b25cac0402d380488d4
jill <jgundaker@gmail.com>
2018-08-18 16:05:39 -0700

    from my road project

--- src/App.js
+++ src/App.js
@@ -1,21 +1,77 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import axios from 'axios'
 class App extends Component {
+
+  state = {
+    venues:[]
+  }
+
+  componentDidMount() {
+    this.getVenues()
+    
+  }
+
+
+  renderMap = () => {
+    loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyAxAlm6hATvEKS6b_vmoEFZwdLMDRpdDcs&callback=initMap")
+    window.initMap = this.initMap
+
+  }
+
+  getVenues =() => {
+    const endPoint = "https://api.foursquare.com/v2/venues/explore?"
+    const parameters = {
+      client_id:"JLIPFSTRQINS3SJWZEMHLLL20U0TYRV2RW5F4NOTEDXYAJVR",
+      client_secret:"R4WNZB2KMVUKJGD5CAGFAUZVJNYJ1YWGILUJ5TN43UZPZ2ZJ",
+      query:"coffee",
+      near:"Phoenix, AZ",
+      v: "20181508"
+    
+
+    }
+    axios.get(endPoint + new URLSearchParams(parameters))
+      .then(response => {
+        this.setState({
+          venues:response.data.response.groups[0].items
+        }, this.renderMap())
+
+      })
+      .catch(error => {
+        console.log("ERROR!!" = error)
+      })
+  }
+
+
+  initMap = () => {
+    const map = new window.google.maps.Map(document.getElementById('map'), {
+      center: {lat: -34.397, lng: 150.644},
+      zoom: 8
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome piolop React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <main>
+        <div id="map"></div>
+      </main>
+     
+
+    ) 
   }
 }
 
+
+function loadScript(url) {
+  var index = window.document.getElementsByTagName("script")[0]
+  var script = window.document.createElement ("script")
+  script.src = url
+  script.async = true
+  script.defer = true
+  index.parentNode.insertBefore(script, index)
+}
+
+
+
+ 
 export default App;
