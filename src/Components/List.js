04e060c324f016f7c93fe6d3e23700dd75f4d719
jill <jgundaker@gmail.com>
2018-08-23 21:04:21 -0700

    component

--- src/Components/List.js
+++ src/Components/List.js
@@ -1,3 +1,21 @@
-import React from 'react';
-
-
+import React, { Component } from "react";
+ 
+class List extends Component {
+  render() {
+    return (
+        <div>
+          
+          
+            <li><a href="/">Home</a></li>
+            <li><a href="/stuff">Stuff</a></li>
+            <li><a href="/contact">Contact</a></li>
+          
+          <div className="content">
+             
+          </div>
+        </div>
+    );
+  }
+}
+ 
+export default List;
