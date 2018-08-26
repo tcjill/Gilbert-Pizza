03b5978b9c1bcb6a166c8e38f56b83c8e77f5a01
jill <jgundaker@gmail.com>
2018-08-26 09:39:41 -0700

    add authfail note

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
+/*<script type="text/javascript">
+      function gm_authFailure() { 
+        alert(`Google Maps Authentication Failed! Map Will not load correctly
+              Please Check your API License Key`)
+      // here define authentication failed message     
+     };
+    </script>*/
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
