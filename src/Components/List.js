f3d7600628b633a9110658ab004ce94408b6779e
jill <jgundaker@gmail.com>
2018-09-03 19:01:45 -0700

    search bar text to venues OK

--- src/Components/List.js
+++ src/Components/List.js
@@ -2,7 +2,46 @@ import React, { Component } from 'react'
 import './style.css'
 
 class List extends Component {
-    
+  state ={
+    query:'',
+    searchedBooks: []
+  }
+  updateQuery = (query) => {
+    this.setState({query});
+  }
+render() {
+   
+    // with destructuring you can do the same
+    // as below but with less typing
+    const {venues} = this.props;
+    console.log(venues)
+     return (
+       <aside className="sidebar">
+        <input 
+            type="text" 
+            placeholder="Search Places"
+            value={this.state.query}
+            onChange={(event) => this.updateQuery(event.target.value)}
+        />
+          <ul className="venue-list-parent">
+          
+            {venues
+              .filter(filtered => filtered.venue.name.toLowerCase().indexOf(this.state.query.toLowerCase()) > -1)
+              .map((item, index) => {
+                return <li className="venue-list-item" key={index}>{item.venue.name}</li>
+            })}
+          </ul>
+        </aside>
+    )
+  }
+}
+export default List
+
+
+
+
+
+
 //indexOfExample() {
   // const listItem = 'Jill Gundaker\'s Pizza';
   // const search = 'Jill';
@@ -31,26 +70,15 @@ class List extends Component {
  // console.log('Filtered State', filteredState)
 
 //}
+/*
+                
+                        <input 
+                            type="text" 
+                            placeholder="Search by title or author"
+                            value={this.state.query}
+                            onChange={(event) => this.updateQuery(event.target.value)}
+                            />
+                   
+                 
 
-render() {
-   
-    // with destructuring you can do the same
-    // as below but with less typing
-    const {venues} = this.props;
-    console.log(venues)
-     return (
-       <aside className="sidebar">
-          <input type="text" className="search-input" placeholder="Search Places"/>
-          <ul className="venue-list-parent">
-            {venues
-              .filter(filtered => filtered.venue.name.indexOf('') > -1)
-              .map((item, index) => {
-                return <li className="venue-list-item" key={index}>{item.venue.name}</li>
-            })}
-          </ul>
-        </aside>
-    )
-  }
-}
-export default List
-
+*/
